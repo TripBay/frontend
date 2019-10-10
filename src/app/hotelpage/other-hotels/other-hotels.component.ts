f7c1e7cc1f29e1a89a1c6a82eadc5d0a8797ec46
James Lester Orfano <resuta566@gmail.com>
2019-10-10 17:28:24 +0800

    Full Responsive HomePage

--- src/app/hotelpage/other-hotels/other-hotels.component.ts
+++ src/app/hotelpage/other-hotels/other-hotels.component.ts
@@ -12,4 +12,33 @@ export class OtherHotelsComponent implements OnInit {
   ngOnInit() {
   }
 
+  images = ['assets/monina1.jpg', 'assets/monina1.jpg',
+             'assets/monina1.jpg', 'assets/monina1.jpg',
+              'assets/monina1.jpg', 'assets/monina1.jpg',
+               'assets/monina1.jpg', 'assets/monina1.jpg',
+               'assets/monina1.jpg'];
+
+  SlideOptions = {
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    lazyLoad: true,
+    responsive:{
+      0:{
+        items:1,
+        nav:true
+      },
+      768:{
+          items:2,
+          nav:true
+      },
+      1080:{
+          items:3,
+          nav:true
+      }
+    }};
+  CarouselOptions = { items: 8, dots: true, nav: true };
 }
